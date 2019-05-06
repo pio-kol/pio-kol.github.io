85c173e6180ddb8a0a7d1ad829058f58f6895846
Piotr Kołacz <pio-kol@users.noreply.github.com>
2019-05-06 19:30:10 +0200

    Update order_forms.js

--- js/order_forms.js
+++ js/order_forms.js
@@ -120,7 +120,7 @@ function Get(url, disabled = false){
         xhr.onreadystatechange = function() {
           if (xhr.readyState === 4) { 
             addAvailableMeetings(JSON.parse(xhr.responseText), disabled);
-            fillInputsFromGetParameters(false);
+            fillInputsFromGetParameters(disabled, false);
           }
         }
         //xhr.setRequestHeader('Content-Type', 'application/json')
