bf1fa317fdfdcb09ebd98d234c47bd151f50ac4c
Piotr Kołacz <pio-kol@users.noreply.github.com>
2019-05-06 15:56:10 +0200

    Update order_forms.js

--- js/order_forms.js
+++ js/order_forms.js
@@ -83,8 +83,9 @@ function readAvailableEvents(){
 }
 
 function Get(yourUrl){
-    var Httpreq = new XMLHttpRequest(); // a new request
-    Httpreq.open("GET",yourUrl,false);
-    Httpreq.send(null);
-    return Httpreq.responseText;          
+    var xhr = new XMLHttpRequest(); // a new request
+    xhr.withCredentials = true
+    xhr.open("GET",yourUrl,true);
+    xhr.send(null);
+    return xhr.responseText;          
 }
