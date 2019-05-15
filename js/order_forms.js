9b3cb19893bfb5aef5d51e6da2a407e655acec8d
Piotr Kołacz <pio-kol@users.noreply.github.com>
2019-05-15 18:57:21 +0200

    Update order_forms.js

--- js/order_forms.js
+++ js/order_forms.js
@@ -143,6 +143,7 @@ function applyDiscount(){
               document.getElementById("lead_price").value = Math.round(document.getElementById("lead_price_original").value * (1-discount_percent/100));
               var meetings = document.getElementById("available_meetings");
               while (meetings.firstChild) {
+                alert(meetings.firstChild.type);
                 if (meetings.firstChild.type != "legend"){
                   meetings.removeChild(meetings.firstChild);
                 }
