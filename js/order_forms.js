0455ee4b6ce071e0d38c2357629d65279ad3d6f2
Piotr Kołacz <pio-kol@users.noreply.github.com>
2019-05-15 18:06:43 +0200

    Update order_forms.js

--- js/order_forms.js
+++ js/order_forms.js
@@ -136,8 +136,8 @@ function applyDiscount(){
         //xhr.withCredentials = true
         xhr.onreadystatechange = function() {
           if (xhr.readyState === 4) { 
-            document.getElementById("senior_price").value = round(document.getElementById("senior_price").value * (1-parseFloat(xhr.responseText)/100));
-            document.getElementById("lead_price").value = round(document.getElementById("lead_price").value * (1-parseFloat(xhr.responseText)/100));
+            document.getElementById("senior_price").value = Math.round(document.getElementById("senior_price").value * (1-parseFloat(xhr.responseText)/100));
+            document.getElementById("lead_price").value = Math.round(document.getElementById("lead_price").value * (1-parseFloat(xhr.responseText)/100));
             alert("Zastosowano zniżkę " + xhr.responseText + "%");
           }
         }
